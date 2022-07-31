2fa6d6fc3cbaf0f3c87d178b5a446c44191bdf6a
yuva20122 <110332119+yuva20122@users.noreply.github.com>
2022-07-31 22:45:34 +0530

    Add files via upload

---
+++ Import files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+pd.read_excel(r'C:\Users\Yuvaraj\Desktop\data1.xlsx')
+
+
+# In[8]:
+
+
+pd.read_csv(r"C:\Users\Yuvaraj\Desktop\data3.csv")
+
+
+# In[11]:
+
+
+pd.read_csv(r'C:\Users\Yuvaraj\Desktop\data2.tsv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
